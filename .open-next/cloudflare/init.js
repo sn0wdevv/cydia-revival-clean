31b3ddb5036141884cc91206a25b378041e9e4d7
sn0w <sn0wrootcore@gmail.com>
2026-05-28 07:33:06 +0200

    tried to fix something

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1779945589708,
+    __BUILD_TIMESTAMP_MS__: 1779945874052,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,

174f0f09bff290f33345a95edd8480b3456be5ab
sn0w <sn0wrootcore@gmail.com>
2026-05-28 07:13:20 +0200

    Add sp

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1779943742134,
+    __BUILD_TIMESTAMP_MS__: 1779944782019,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,

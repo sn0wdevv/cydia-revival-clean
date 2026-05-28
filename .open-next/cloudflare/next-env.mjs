8121c02e6e2fc8a7daf43c4a41f725f67c8d05e9
sn0w <sn0wrootcore@gmail.com>
2026-05-28 07:48:32 +0200

    tried to fix something

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://jcixksfonxoqpzfrfumh.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_O8K3snD1Ieyiz-smfKhnoA_ibVkG3U0"};
-export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://jcixksfonxoqpzfrfumh.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_O8K3snD1Ieyiz-smfKhnoA_ibVkG3U0"};
-export const test = {};
